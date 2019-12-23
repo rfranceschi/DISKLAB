c4fa2b7afa3112de420eafd7ad10b93ba67b1eaa
Riccardo Franceschi <franceschiriccardo93@gmail.com>
2019-12-23 16:08:36 +0000

    Delete script.py

--- mydiskmodels/viscous/script.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-n           = 20
-amin        = 1   # 0.1 micron
-amax        = 20   # 10 micron
-agraini     = np.linspace(amin-1,amax,n+1)
-agrain      = 0.5 * ( agraini[1:] + agraini[:-1] )
-
-print(agraini[1:])
-print(agraini[:-1])
